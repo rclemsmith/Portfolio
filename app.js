f64d37395bb03775ea222250afa7ce43e30ca278
Clement Smith R <rclemsmith@gmail.com>
2020-11-27 14:16:35 +0530

    HTTPS

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var express = require("express");
+var fs = require("fs");
+const https = require('https');
+
+const cert = fs.readFileSync("rclemsmith_me.crt");
+const ca = fs.readFileSync('rclemsmith_me.ca-bundle');
+const key = fs.readFileSync('private.key');
 var app = express();
+
+const httpsOptions = {cert, ca, key};
+const httpsServer = https.createServer(httpsOptions, app);
 app.use(express.static(__dirname + '/'));
 
+app.use((req, res, next) => {
+    console.log("Incoming");
+    if(req.protocol === 'http') {
+      res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+    next();
+ });
+
+
 app.get('/', function (req, res) {
+    console.log("Incoming");
     res.sendFile("index.html");
 });
 
-app.listen(80, () => console.log("Server Started"));
+httpsServer.listen(80, () => console.log("Server Started"));
